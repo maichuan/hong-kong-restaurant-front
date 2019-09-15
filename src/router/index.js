e364205eefbbbfd13d2e58b0d130932f7f433df9
dacharat <dacharat.p@ku.th>
2019-09-15 23:31:20 +0700

    :tada: add router

---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Route } from 'react-router-dom'
+
+import Home from '../pages/Home'
+
+const routes = [
+  {
+    path: '/',
+    exact: true,
+    component: Home,
+  },
+]
+
+export default () => (
+  <>
+    {routes.map((route, i) => (
+      <Route key={i} {...route} />
+    ))}
+  </>
+)
